676f7ad44cb3b0f22a63d72db55283b6e90e94d2
hdragon8xxx <hdragon8xxxx@gmail.com>
2024-08-25 23:16:36 +0700

    update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -52,7 +52,13 @@ export const authOptions: AuthOptions = {
   debug: process.env.NODE_ENV === "development",
   session: {
     strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+    updateAge: 24 * 60 * 60, // 24 hours
   },
+  jwt: {
+    maxAge: 60 * 60 * 24 * 30, // 30 days
+  },
+
   secret: process.env.NEXTAUTH_SECRET,
 };
 
